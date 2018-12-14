a4e46ca0fb0becf3ea6f62d2014a8494eb661c89
ThomasP <trolldu25@live.fr>
2018-12-14 14:13:41 +0100

    Fix RunTest

--- RunTest.php
+++ RunTest.php
@@ -161,6 +161,7 @@ function runTest() {
 				
 						
 }
+      runTest();
 
 
 		
--- index.php
+++ index.php
@@ -73,7 +73,7 @@ input[type=submit] {
 <p>Entrez votre point de départ et d'arrivée</p>
 
 <!--Make sure the form has the autocomplete function switched off:-->
-<form autocomplete="off" action="Main.php">
+<form autocomplete="off" action="RunTest.php">
   <div class="autocomplete" style="width:300px;">
     <input id="startPt" type="text" name="a" placeholder="Point de départ">
     <input id="endPt" type="text" name="b" placeholder="Point d'arrivée'">
